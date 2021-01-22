59858d8499279be6323368b80f2df37a28d32a2f
Kadir Teke <Kadirtk1344@hotmail.com>
2021-01-22 14:28:01 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T,J>
+    {
+        T[] keyArray;
+        T[] tempKey;
+        J[] valueArray;
+        J[] tempValue;
+
+        public MyDictionary()
+        {
+            keyArray = new T[0];
+            valueArray = new J[0];
+        }
+
+        public void Add(T key,J value)
+        {
+            tempKey = keyArray;
+            tempValue = valueArray;
+
+            keyArray = new T[keyArray.Length + 1];
+            valueArray = new J[valueArray.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keyArray[i] = tempKey[i];
+                valueArray[i] = tempValue[i];
+            }
+            keyArray[keyArray.Length - 1] = key;
+            valueArray[valueArray.Length - 1] = value;
+        }
+        public T[] Keys
+        {
+            get { return keyArray; }
+        }
+        public J[] Values
+        {
+            get { return valueArray; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> city = new MyDictionary<string, string>();
+            city.Add("TR","Adana");
+            city.Add("JP", "Akihabara");
+            city.Add("US", "Miami");
+            city.Add("CA", "Toronto");
+            city.Add("UK", "London");
+            city.Add("GR", "Berlin");
+            city.Add("FR", "Paris");
+            city.Add("RU", "Moscow");
+            city.Add("HU", "Budapest");
+
+            foreach (var item in city.Keys)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
